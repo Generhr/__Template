051518631ef44da3b650f25e222198881ca915d2
Onimuru <onimuru@gmail.com>
2023-07-22 17:56:25 +0100

    Added some VS Code settings to the batch file to symlink

--- Tools/Symlink.bat
+++ Tools/Symlink.bat
@@ -2,6 +2,9 @@
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
+mklink %cd%\..\.vscode\c_cpp_properties.json %USERPROFILE%\Code\C++\__Template\.vscode\c_cpp_properties.json
+mklink %cd%\..\.vscode\settings.json %USERPROFILE%\Code\C++\__Template\.vscode\settings.json
+
 mklink %cd%\..\.clang-format %USERPROFILE%\Code\C++\__Template\.clang-format
 mklink %cd%\..\.clang-tidy %USERPROFILE%\C++\__Template\.clang-tidy
 mklink %cd%\..\.cmake-format.yaml %USERPROFILE%\Code\C++\__Template\.cmake-format.yaml

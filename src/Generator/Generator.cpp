1145e0fc7bb9be0cbc5b766a05a76c5bbe6e5684
Onimuru <onimuru@gmail.com>
2023-07-29 05:37:24 +0100

    Fixed some bad includes

--- src/Generator/Generator.cpp
+++ src/Generator/Generator.cpp
@@ -5,7 +5,6 @@
 #include <random>     // for random_device, mt19937
 #include <string>     // for to_string
 #include <vector>     // for vector, _Vector_const_iterator, _Vector_iterator
-#include <xstring>    // for operator<<
 
 std::stringstream& Generator::generate(std::stringstream& stream, const int range) {
     std::vector<int> data(range);
